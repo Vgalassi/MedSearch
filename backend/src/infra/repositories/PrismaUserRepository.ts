30729f2cf4cb2193c91ad70f53f2cea6b5c39659
VGalassi <viniciusgalassi@gmail.com>
2026-04-19 19:04:19 -0300

    refactor: add clean architecture folders

---
+++ backend/src/infra/repositories/PrismaUserRepository.ts
@@ -0,0 +1,24 @@
+import { Prisma } from "@prisma/client/extension";
+
+
+export class UserRepository{
+    async createUser(){
+
+    }
+
+    async getUser(){
+
+    }
+
+    async updateUser(){
+
+    }
+
+    async deleteUser(){
+
+    }
+    
+
+
+
+}
